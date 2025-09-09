587dd22f62b1d747511d0492436ff4fef852266f
ElliotJ-Git <elliot.jost@gmail.com>
2025-09-09 13:48:04 -0600

    Week-2 Assignment Completion

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import GitHubPage from "./student-info";
+
+
+export default function Page() {
+    
+    return(
+        <main>
+            <h1 className="text-3x1">Shopping List</h1>
+            <GitHubPage/>
+        </main>
+    )
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function GitHubPage(Page){
+
+
+    return(
+        <main>
+            <p>Elliot Jost</p>
+            <p>
+            <Link href="https://github.com/Rokko1egend/cprg306-assignments">Github Page Link</Link>
+            </p>
+        </main>
+    );
+}
+
